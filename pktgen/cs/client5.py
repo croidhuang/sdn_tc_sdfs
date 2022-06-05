b02fb4d37b339afec39728010a8ac473fef3c28c
croidhuang <90076286+croidhuang@users.noreply.github.com>
2022-06-05 22:44:24 +0800

    Add files via upload

--- pktgen/cs/client5.py
+++ pktgen/cs/client5.py
@@ -25,7 +25,7 @@ topo_SLICENDICT,     \
 HISTORYTRAFFIC
 
 from exp_utils.exp_utils import \
-G_to_M, num_to_hostmac,num_to_hostipv4
+G_to_M, num_to_hostmac,num_to_hostipv4,clienttraffictype_to_L4port
 
 time_ctrl=False
 
@@ -36,6 +36,7 @@ hostid = os.path.basename(inspect.getfile(inspect.currentframe()))
 hostid = int(hostid[start:end])
 
 random.seed(RANDOM_SEED_NUM)
+runcnt = 0
 
 class Counter(dict):
     def __missing__(key):
@@ -44,11 +45,11 @@ class Counter(dict):
 def host_traffic_gen(hostid):
     gen_ctrl = False
 
-    def payload_gen(length):    
+    def payload_gen(length):
         chars=string.ascii_letters
         payload="".join(random.choice(chars) for i in range(length))
         return payload
-        
+    
     isd_dict={i:{(G_to_M(s),G_to_M(d)):[] \
               for d in topo_G.nodes() for s in topo_G.nodes()}\
               for i in topo_SLICENDICT.keys()}
@@ -64,11 +65,11 @@ def host_traffic_gen(hostid):
             if srcid == hostid:
                 gen_ctrl = True
                 length=int(ONE_PKT_SIZE[SLICE_TRAFFIC_MAP[i]])
-                L4sport=RandShort()
+                L4sport=int(clienttraffictype_to_L4port(i,hostid))+int(srcid)*10+int(dstid)
                 while length > 0 :
                     pkt = Ether(src=num_to_hostmac(srcid), dst=num_to_hostmac(dstid))/   \
                           IP(src=num_to_hostipv4(srcid), dst=num_to_hostipv4(dstid))/    \
-                          UDP(sport=L4sport,dport=5001)        
+                          UDP(sport=L4sport,dport=L4sport)
                     if length > 1500:
                         payload = 1500-len(pkt)
                     else:
@@ -86,14 +87,20 @@ def host_traffic_gen(hostid):
 
 flow = host_traffic_gen(hostid)
 
+
+
+
 def send_flow(i,srcid,dstid):
-    print(f"client {str(hostid)}: ready")
-    if GOGO_TIME:
-        time.sleep(FIRST_TIME_SLEEP*i)   
     myinterface="h"+str(hostid)+"-eth0"
     myL2socket = conf.L2socket(iface=myinterface)
-    print(f"INNTER_ARRIVAL_TIME[{i}]={INNTER_ARRIVAL_TIME[i]} \t NUM_PKT[{i}]={NUM_PKT[i]*len(flow[i][(srcid,dstid)])}")
+    #print(f"client {str(hostid)} INNTER_ARRIVAL_TIME[{i}]={INNTER_ARRIVAL_TIME[i]} \t NUM_PKT[{i}]={NUM_PKT[i]*len(flow[i][(srcid,dstid)])}")
     sendp(flow[i][(srcid,dstid)],count=NUM_PKT[i],iface=myinterface,verbose=False,socket=myL2socket)
+   
+def pre_send_flow(i,srcid,dstid):
+    myinterface="h"+str(hostid)+"-eth0"
+    myL2socket = conf.L2socket(iface=myinterface)
+    #print(f"client {str(hostid)} INNTER_ARRIVAL_TIME[{i}]={INNTER_ARRIVAL_TIME[i]} \t NUM_PKT[{i}]=pre len={len(flow[i][(srcid,dstid)])}")
+    sendp(flow[i][(srcid,dstid)],count=len(flow[i][(srcid,dstid)]),iface=myinterface,verbose=False,socket=myL2socket)
     
     """
     prevt=0
@@ -101,7 +108,7 @@ def send_flow(i,srcid,dstid):
     sumFast=0
     sumL2=0
     sumL3=0
-    for rrr in range(10):    
+    for rrr in range(10):
         myL2socket = conf.L2socket(iface=myinterface)
         currt=time.time()
         prevt=currt 
@@ -134,7 +141,7 @@ def send_flow(i,srcid,dstid):
     if hostid == 2:
         os.system("iperf -f MBytes -c 10.0.0.1 -p 5001 -u -b 10M")
     """
-    
+
     #prevt=0
     """
     myL3socket = conf.L3socket(iface=myinterface)
@@ -151,13 +158,13 @@ def send_flow(i,srcid,dstid):
     sendp(flow[i][(srcid,dstid)],iface=myinterface,verbose=False,socket=myL2socket)
     currt=time.time()
     print(f"inter1={currt-prevt}")
-    """        
+    """
 
 
 
 def sniff_flow(hostid):
     interface_port = "h"+str(hostid)+"-eth0"
-    sniff(iface=interface_port,prn=lambda pkt: pkt.summary())
+    sniff(iface=interface_port,prn=lambda pkt:pkt.summary())
 
 
 def cnt_flow():
@@ -167,39 +174,45 @@ def cnt_flow():
             for e,v in slicedict.items():
                 srcid=e[0]
                 dstid=e[1]
-                if len(v) != 0: 
+                if len(v) != 0:
                     processlist.append(pool.apply_async(send_flow,(i,srcid,dstid)))
         pool.close()
         pool.join()
 
+def pre_cnt_flow():
+    processlist=[]
+    with Pool(processes=4) as prepool:
+        for i,slicedict in flow.items():
+            for e,v in slicedict.items():
+                srcid=e[0]
+                dstid=e[1]
+                if len(v) != 0:
+                    processlist.append(prepool.apply_async(pre_send_flow,(i,srcid,dstid)))
+        prepool.close()
+        prepool.join()
+
 
 def main():
     if flow == None:
-        print(f"client {str(hostid)}: nothing to send")
+        print(f"client {str(hostid)}:nothing to send")
     else:
-        print(f"client {str(hostid)}: start")
+        print(f"client {str(hostid)}:start")
         cnt_flow()
 
-
-    """
-    clientdict={}
-    for i,slicedict in flow.items():
-        for e,v in slicedict.items():
-                srcid=e[0]
-                dstid=e[1]
-                if len(v) != 0: 
-                    clientdict[i] = Process(target = send_flow, args = (i,srcid,dstid))
-    for k,v in clientdict.items():
-        v.start()
-    for k,v in clientdict.items():
-        v.join()
-    """
-
+def pre_main():
+    if flow == None:
+        print(f"client {str(hostid)}:nothing to send")
+    else:
+        print(f"client {str(hostid)}:prestart")
+        pre_cnt_flow()
 
 if __name__ == "__main__":
     if GOGO_TIME:
-        timestamp = float(time.time())
-        time.sleep(GOGO_TIME-timestamp-10)
+        pre_main()
+        time.sleep(GOGO_TIME-float(time.time())-60)
+    else:
+        pre_main()
+        time.sleep(60)
     while (timestamp := float(time.time())) < GOGO_TIME:
         pass
     main()
