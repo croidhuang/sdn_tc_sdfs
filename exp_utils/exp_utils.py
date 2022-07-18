88361e091b7b4b4fab5d11ded28492b593831bb7
croidhuang <90076286+croidhuang@users.noreply.github.com>
2022-07-18 19:08:18 +0800

    Add files via upload

--- exp_utils/exp_utils.py
+++ exp_utils/exp_utils.py
@@ -107,10 +107,10 @@ def num_to_switchmac(switch_num):
     return switch_mac_addr
 
 def clienttraffictype_to_L4port(traffictype,client):
-    L4port=traffictype*1000+client*100
+    L4port=traffictype*10000+client*100
     return L4port
 
 def L4port_to_clienttraffictype(L4port):
-    L4port=str(L4port).zfill(4)
+    L4port=str(L4port).zfill(5)
     traffictype=L4port[0]
     return traffictype
