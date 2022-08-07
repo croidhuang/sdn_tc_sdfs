c81839357313f1af119c317b60fa7d05b8a8f6f3
croidhuang <90076286+croidhuang@users.noreply.github.com>
2022-08-07 21:15:34 +0800

    Add files via upload

--- exp_iter.py
+++ exp_iter.py
@@ -5,7 +5,7 @@ import os
 
 a=["algo",       "sp_sortC"]
 b=[False,        "MAX"]
-c=["routing",    "fake", ]
+c=["routing",    "scheduling_routing", ]
 d=["est_avg",    "port"]
 
 
@@ -26,8 +26,33 @@ for aa in a:
                 username='croid'
                 homedir = os.path.expanduser('~'+username)
                 try:
+                    Popen(["mn","-c"],cwd=homedir)
                     Popen(["python3","-u",'mininet/custom/custom_example_nxtomini.py'],cwd=homedir)
                 except:
                     print('change exp_iter.py username')
                 
                 time.sleep(4*60+240)  
+"""                
+
+for i in range(3):
+    exp_iter={}
+    exp_iter['ROUTING_TYPE'] = "algo"
+    exp_iter['SCHEDULER_TYPE'] = False
+    exp_iter['EXP_TYPE'] =  "routing"
+    exp_iter['DYNBW_TYPE'] = "est_avg"
+    timestamp = time.time()+(3*60)
+    struct_time = time.localtime(timestamp)
+    exp_iter['timestring'] = time.strftime("%Y-%m-%d %H:%M:%S", struct_time)        
+    json.dump(exp_iter, open("exp_iter.txt","w"))
+
+    username='croid'
+    homedir = os.path.expanduser('~'+username)
+    try:
+        Popen(["mn","-c"],cwd=homedir)
+        Popen(["python3","-u",'mininet/custom/custom_example_nxtomini.py'],cwd=homedir)        
+    except:
+        print('change exp_iter.py username')
+    
+    time.sleep(4*60+240)  
+
+"""    
