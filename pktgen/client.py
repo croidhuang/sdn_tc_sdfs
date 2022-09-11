29130cb41ec3aef6ea2ad321c820b2f41640a7ce
croidhuang <90076286+croidhuang@users.noreply.github.com>
2022-09-11 23:01:13 +0800

    Add files via upload

--- pktgen/client.py
+++ pktgen/client.py
@@ -1,3 +1,4 @@
+from socket import timeout
 from tabnanny import verbose
 from scapy.all import *
 from scapy.layers.inet import Ether, IP, UDP, TCP
@@ -10,13 +11,15 @@ import time
 from pathlib import Path
 import pprint
 import copy
-
+import socket
 
 from pebble import ProcessPool
-from multiprocessing import Pool,cpu_count,TimeoutError
+from multiprocessing import Pool, cpu_count, TimeoutError
+
+from subprocess import Popen
 
 import sys
-sys.path.insert(1,"./")
+sys.path.insert(1, "./")
 from exp_config.exp_config import \
 SLICE_TRAFFIC_MAP, NUM_PKT,     \
 SCHEDULER_TYPE, EXP_TYPE, RANDOM_SEED_NUM,     \
@@ -24,10 +27,10 @@ GOGO_TIME, TOTAL_TIME, READPKT_TIME, BETWEEN_HISTORY_EXTRA_TIME,     \
 ONE_PKT_SIZE, INNTER_ARRIVAL_TIME,     \
 topo_G,     \
 topo_SLICENDICT,     \
-HISTORYTRAFFIC,EXTRATRAFFIC
+HISTORYTRAFFIC, EXTRATRAFFIC
 
 from exp_utils.exp_utils import \
-G_to_M, M_to_G ,num_to_hostmac,num_to_hostipv4,clienttraffictype_to_L4port
+G_to_M, M_to_G , num_to_hostmac, num_to_hostipv4, clienttraffictype_to_L4port
 
 
 
@@ -43,12 +46,65 @@ random.seed(RANDOM_SEED_NUM)
 
 #生成flow
 def host_traffic_gen(hostid, dict_ctrl):
-    gen_ctrl = False
-
-    def payload_gen(length):
+    def payload_gen(per_pkt_length):
         chars = string.ascii_letters
-        payload = "".join(random.choice(chars) for i in range(length))
+        payload = "".join(random.choice(chars) for i in range(per_pkt_length))
         return payload
+
+    def trafficlist_gen(TRAFFIC, isd_dict, listenport_dict, gen_ctrl):
+        for i, slicedict in TRAFFIC.items():
+            for k, v in slicedict.items():
+                if v == 0:
+                    continue
+
+                srcid = G_to_M(k[0])
+                dstid = G_to_M(k[1])
+
+                if dstid == hostid:
+                    gen_ctrl = True
+                    L4sport = clienttraffictype_to_L4port(i, hostid, dstid)
+                    """
+                    scapy
+                    """
+                    """
+                    #server
+                    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    s.bind((num_to_hostipv4(dstid), L4sport))
+                    """
+
+                    listenport_dict[(str(L4sport))] = 1
+
+                if srcid == hostid:
+                    gen_ctrl = True
+                    pkt_length = int(v*float(INNTER_ARRIVAL_TIME[i]/1))
+                    L4sport = clienttraffictype_to_L4port(i, hostid, dstid)
+
+                    """
+                    scapy
+                    """
+                    """
+                    #client
+                    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    s.connect((num_to_hostipv4(dstid), L4sport))
+                    """
+
+                    pkt = Ether(src = num_to_hostmac(srcid), dst = num_to_hostmac(dstid))/   \
+                    IP(src = num_to_hostipv4(srcid), dst = num_to_hostipv4(dstid))/    \
+                    UDP(sport = L4sport, dport = L4sport)
+
+                    pkt_total = int(pkt_length/1500)+1
+                    per_pkt_length = int(pkt_length / pkt_total)
+                    for pi in range(pkt_total):
+                        payload = payload_gen(per_pkt_length)
+                        addpkt = pkt / payload
+                        isd_dict[i][(srcid, dstid)].append(addpkt)
+
+        if gen_ctrl == False:
+            return None, None, gen_ctrl
+        else:
+            return isd_dict, listenport_dict, gen_ctrl
+
+    gen_ctrl = False
 
     hostlist = []
     for s in topo_G.nodes():
@@ -59,192 +115,125 @@ def host_traffic_gen(hostid, dict_ctrl):
         except:
             pass
 
-    isd_dict = {i:{(G_to_M(s),G_to_M(d)):[] \
+    isd_dict = {i:{(G_to_M(s), G_to_M(d)):[] \
               for d in hostlist for s in hostlist}\
               for i in topo_SLICENDICT.keys()}
 
+    listenport_dict = {}
+
     if dict_ctrl == "history" or dict_ctrl == "both":
-        for i,slicedict in HISTORYTRAFFIC.items():
-            for k,v in slicedict.items():
-                if v == 0:
-                    continue
-
-                srcid = G_to_M(k[0])
-                dstid = G_to_M(k[1])
-
-                if srcid == hostid:
-                    gen_ctrl = True
-                    length = int(v*float(INNTER_ARRIVAL_TIME[i]/1))
-                    L4sport = int(clienttraffictype_to_L4port(i,hostid))+int(dstid)
-                    while length > 0 :
-                        pkt = Ether(src = num_to_hostmac(srcid), dst = num_to_hostmac(dstid))/   \
-                            IP(src = num_to_hostipv4(srcid), dst = num_to_hostipv4(dstid))/    \
-                            UDP(sport = L4sport,dport = L4sport)
-
-                        if length > int(1500*1.5):
-                            payload = (1500-len(pkt))
-                            length = length - 1500
-                            #per1500
-                            data = payload_gen(payload)
-                            pkt = pkt/data
-                            isd_dict[i][(srcid,dstid)].append(pkt)
-
-                        elif length > len(pkt):
-                            payload = int((length-len(pkt)*2)/2)
-
-                            #splite 750*2
-                            data1 = payload_gen(payload)
-                            data2 = payload_gen(payload)
-                            pkt1 = pkt/data1
-                            pkt2 = pkt/data2
-                            isd_dict[i][(srcid,dstid)].append(pkt1)
-                            isd_dict[i][(srcid,dstid)].append(pkt2)
-                            length = -1
-                        else:
-                            #header only
-                            isd_dict[i][(srcid,dstid)].append(pkt)
-                            length = length - 1500
-
-
-
+        TRAFFIC = copy.deepcopy(HISTORYTRAFFIC)
+        isd_dict, listenport_dict, gen_ctrl = trafficlist_gen(TRAFFIC, isd_dict, listenport_dict, gen_ctrl)
     if dict_ctrl == "extra" or dict_ctrl == "both":
-        for i,slicedict in EXTRATRAFFIC.items():
-            for k,v in slicedict.items():
-                if v == 0:
-                    continue
-
-                srcid = G_to_M(k[0])
-                dstid = G_to_M(k[1])
-
-                if srcid == hostid:
-                    gen_ctrl = True
-                    length = int(v*float(INNTER_ARRIVAL_TIME[i]/1))
-                    L4sport = int(clienttraffictype_to_L4port(i,hostid))+int(dstid)
-                    while length > 0 :
-                        pkt = Ether(src = num_to_hostmac(srcid), dst = num_to_hostmac(dstid))/   \
-                            IP(src = num_to_hostipv4(srcid), dst = num_to_hostipv4(dstid))/    \
-                            UDP(sport = L4sport,dport = L4sport)
-
-                        if length > int(1500*1.5):
-                            payload = (1500-len(pkt))
-                            length = length - 1500
-                            #per1500
-                            data = payload_gen(payload)
-                            pkt = pkt/data
-                            isd_dict[i][(srcid,dstid)].append(pkt)
-
-                        elif length > len(pkt):
-                            payload = int((length-len(pkt)*2)/2)
-
-                            #splite 750*2
-                            data1 = payload_gen(payload)
-                            data2 = payload_gen(payload)
-                            pkt1 = pkt/data1
-                            pkt2 = pkt/data2
-                            isd_dict[i][(srcid,dstid)].append(pkt1)
-                            isd_dict[i][(srcid,dstid)].append(pkt2)
-                            length = -1
-                        else:
-                            #header only
-                            isd_dict[i][(srcid,dstid)].append(pkt)
-                            length = length - 1500
+        TRAFFIC = copy.deepcopy(EXTRATRAFFIC)
+        isd_dict, listenport_dict, gen_ctrl = trafficlist_gen(TRAFFIC, isd_dict, listenport_dict, gen_ctrl)
 
     if gen_ctrl == False:
-        return None
+        return None, None, gen_ctrl
     else:
-        return isd_dict
+        return isd_dict, listenport_dict, gen_ctrl
 
 # 三種flow
-history_flow = host_traffic_gen(hostid,"history")
-extra_flow = host_traffic_gen(hostid,"extra")
-flow = host_traffic_gen(hostid,"both")
+history_flow, listenport_dict, gen_ctrl = host_traffic_gen(hostid, "history")
+extra_flow, listenport_dict, gen_ctrl = host_traffic_gen(hostid, "extra")
+replay_flow, listenport_dict, gen_ctrl = host_traffic_gen(hostid, "both")
 
-# sniff備用
-def sniff_flow(hostid):
-    interface_port = "h"+str(hostid)+"-eth0"
-    sniff(iface = interface_port,prn = lambda pkt:pkt.summary())
+# sniff
+def sniff_flow(listen_port):
+    #interface_port = "h"+str(hostid)+"-eth0"
+    #sniff(iface = interface_port, prn = lambda pkt:pkt.summary())
+    print(['iperf', '-s', '-u', '-i', str(1),'-p',str(listen_port)])
+    Popen(['iperf', '-s', '-u', '-i', str(1),'-p',str(listen_port)])
 
 
 #發送flow
 """
-###sendp(flow[i][(srcid,dstid)],count = 10,iface = myinterface,verbose = False,socket = myL2socket)
-###send(flow[i][(srcid,dstid)],count = 10,iface = myinterface,verbose = False,socket = myL3socket)
-###sendpfast(flow[i][(srcid,dstid)],loop = 10,iface = myinterface,file_cache = True)
+###sendp(flow[i][(srcid, dstid)], count = 10, iface = myinterface, verbose = False, socket = myL2socket)
+###send(flow[i][(srcid, dstid)], count = 10, iface = myinterface, verbose = False, socket = myL3socket)
+###sendpfast(flow[i][(srcid, dstid)], loop = 10, iface = myinterface, file_cache = True)
 """
-def send_flow(i,srcid,dstid,timestamp):
+
+
+def send_flow(i, srcid, dstid, timestamp, flowtype_ctrl):
+   
+    if flowtype_ctrl == "history":
+        flow = copy.deepcopy(history_flow)
+    elif flowtype_ctrl == "extra":
+        flow = copy.deepcopy(extra_flow)
+    elif flowtype_ctrl == "replay":
+        flow = copy.deepcopy(replay_flow)
     
     strpkt=''
-    for pkt in flow[i][(srcid,dstid)]:
-        strpkt=strpkt+str(len(pkt))+'_'
+    pkt_length = 0
+    for pkt in flow[i][(srcid, dstid)]:
+        strpkt = strpkt+str(len(pkt))+'_'
+        pkt_length += int(len(pkt))
     print(f"client {str(hostid)}:start\t{strpkt}")
-    myinterface = "h"+str(hostid)+"-eth0"
-    myL2socket = conf.L2socket(iface = myinterface)
-    while (timestamp := time.time()) < GOGO_TIME:
-        pass
-    sendp(flow[i][(srcid,dstid)],inter = INNTER_ARRIVAL_TIME[i],count = NUM_PKT[i],iface = myinterface,verbose = False, socket = myL2socket,realtime=False,return_packets=False)
+    
+    """
+    scapy
+    """
+    #myinterface = "h"+str(hostid)+"-eth0"
+    #myL2socket = conf.L2socket(iface = myinterface)
+    #sendp(flow[i][(srcid, dstid)], iface = myinterface, socket = myL2socket, inter = INNTER_ARRIVAL_TIME[i], count = NUM_PKT[i], verbose = False, realtime=False)
 
-def pre_send_history_flow(i,srcid,dstid,timestamp):
-    print(f"client {str(hostid)}:prehistorystart")
-    myinterface = "h"+str(hostid)+"-eth0"
-    myL2socket = conf.L2socket(iface = myinterface)
-    sendp(history_flow[i][(srcid,dstid)],count = 1,iface = myinterface,verbose = False,socket = myL2socket)
-    print(str("client"+ str(hostid)+":prehistorydone "+str(time.time()-timestamp)+ "s"))
+    """
+    iperf
+    """    
+    
+    dst_ip = flow[i][(srcid, dstid)][0][IP].dst    
+    dst_port = flow[i][(srcid, dstid)][0][UDP].dport
+    if flowtype_ctrl == "history":
+        iperftime = 10
+    elif flowtype_ctrl == "extra":
+        iperftime = 10
+    elif flowtype_ctrl == "replay":
+        iperftime = TOTAL_TIME
+        while (timestamp := time.time()) < GOGO_TIME:
+            pass
+    #Popen(['iperf', '-c', str(dst_ip), '-p', str(dst_port), '-u', '-i', str(round(INNTER_ARRIVAL_TIME[i], 3)), '-t', str(iperftime), '-l', str(int(pkt_length))])
+    Popen(['iperf', '-c', str(dst_ip), '-p', str(dst_port), '-u', '-i', str(round(INNTER_ARRIVAL_TIME[i], 3)), '-t', str(iperftime), '-b', str(int(pkt_length)*8/INNTER_ARRIVAL_TIME[i])])
 
-def pre_send_extra_flow(i,srcid,dstid,timestamp):
-    print(f"client {str(hostid)}:preextrastart")
-    myinterface = "h"+str(hostid)+"-eth0"
-    myL2socket = conf.L2socket(iface = myinterface)
-    sendp(extra_flow[i][(srcid,dstid)],count = 1,iface = myinterface,verbose = False,socket = myL2socket)
-    print(str("client"+ str(hostid)+":preextradone "+str(time.time()-timestamp)+ "s"))
+    print(f'client {str(hostid)}:done\t{str((time.time()-timestamp))}s')
 
 
 #multithread開生成中有生成的flow
-def pre_history_flow(timestamp):
-    with ProcessPool() as pre_history_pool:
-        for i,slicedict in history_flow.items():
-            for e,v in slicedict.items():
-                srcid = e[0]
-                dstid = e[1]
-                if len(v) != 0:
-                    timeout = 10
-                    pre_history_pool.schedule(pre_send_history_flow, (i,srcid,dstid,timestamp), timeout = timeout)
-        pre_history_pool.close()
-        pre_history_pool.join()
+def cnt_flow(timestamp, flow, flowtype_ctrl):
 
-def pre_extra_flow(timestamp):
-    with ProcessPool() as pre_extra_pool:
-        for i,slicedict in extra_flow.items():
-            for e,v in slicedict.items():
-                srcid = e[0]
-                dstid = e[1]
-                if len(v) != 0:
-                    timeout = 10
-                    pre_extra_pool.schedule(pre_send_extra_flow, (i,srcid,dstid,timestamp), timeout = timeout)
-        pre_extra_pool.close()
-        pre_extra_pool.join()
+    if flowtype_ctrl == "history":
+        timeout = 10
+    elif flowtype_ctrl == "extra":
+        timeout = 10
+    elif flowtype_ctrl == "replay":
+        timeout = GOGO_TIME-time.time()+TOTAL_TIME
 
-def cnt_flow(timestamp):
     with ProcessPool() as pool:
-        for i,slicedict in flow.items():
-            for e,v in slicedict.items():
+        for i, slicedict in flow.items():
+            for e, v in slicedict.items():
                 srcid = e[0]
                 dstid = e[1]
-                if len(v) != 0:
-                    timeout = TOTAL_TIME
-                    pool.schedule(send_flow, (i,srcid,dstid,timestamp), timeout = timeout)
+                if len(v) != 0:                    
+                    pool.schedule(send_flow, (i, srcid, dstid, timestamp, flowtype_ctrl), timeout = timeout)
+
+        print(listenport_dict)
+        for listen_port in listenport_dict.keys():            
+            dst_port = listen_port
+            pool.schedule(sniff_flow, (listen_port, ), timeout = timeout)
         pool.close()
         pool.join()
+
+
 """
 # 原生pool寫法留存後來改用pebble套件幫忙管理timeout強制關閉
 def cnt_flow():
     processlist=[]
     with Pool(processes=cpu_count()) as pool:
-        for i,slicedict in flow.items():
-            for e,v in slicedict.items():
+        for i, slicedict in flow.items():
+            for e, v in slicedict.items():
                 srcid=e[0]
                 dstid=e[1]
                 if len(v) != 0:
-                    processlist.append(pool.apply_async(send_flow,(i,srcid,dstid)))
+                    processlist.append(pool.apply_async(send_flow, (i, srcid, dstid)))
         pool.close()
         pool.join()
 """
@@ -252,22 +241,22 @@ def cnt_flow():
 #整個flow沒有的話不執行
 def main(GOGO_TIME):
     timestamp = GOGO_TIME
-    if flow == None:
+    if replay_flow == None and listenport_dict == None:
         print(f"client {str(hostid)}:nothing to send")
     else:
-        cnt_flow(timestamp)
+        cnt_flow(timestamp, replay_flow, flowtype_ctrl="replay")
 
 def pre_main():
     timestamp = time.time()
     if history_flow == None:
         print(f"client {str(hostid)}:nothing to presend")
     else:
-        pre_history_flow(timestamp)
+        cnt_flow(timestamp, history_flow, flowtype_ctrl="history")
     time.sleep(BETWEEN_HISTORY_EXTRA_TIME)
     if extra_flow == None:
         print(f"client {str(hostid)}:nothing to extrasend")
     else:
-        pre_extra_flow(timestamp)
+        cnt_flow(timestamp, extra_flow, flowtype_ctrl="extra")
 
 
 # 主要執行
